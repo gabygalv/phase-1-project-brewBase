4e9214e66b29e0a4adb6faba50345405d16349e0
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-13 15:08:12 -0700

    changed api link, rating dotd not working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-const brewAPI = "https://api.openbrewerydb.org/breweries";
-const listContainer = document.querySelectorAll('.brew_list');
-const byId = (id) => document.getElementById(id);
+const brewAPI = "https://api.openbrewerydb.org/breweries/random?size=10";
+const listContainer = document.querySelector('.brewery_list');
 const upvote = document.querySelector('.dotd_like');
 const downvote = document.querySelector('.dotd_dislike');
-let rating = document.querySelector('.count');
+let rating = document.querySelector('.dotd_rating');
 
 fetch(brewAPI)
  .then(res => res.json())
@@ -22,32 +21,52 @@ fetch(brewAPI)
     breweryName.innerText = brewery.name;
     // console.log(breweryName);
     breweryName.addEventListener('click', () => breweryDetails(brewery));
-    //listContainer.appendChild(breweryName);
+    listContainer.appendChild(breweryName);
 }
 
+//will fill in when we have a div for brewery details
 function breweryDetails(brewery) {
     //  console.log(brewery);
     const breweryName = ''
     const breweryType =''
     const breweryAddress = ''
     const breweryPhone = ''
-
-
 };
 
 /////////////////////////
 //DOTD stuff//
 /////////////////////////
+
+fetch("http://localhost:3000/dotd/1")
+.then(res => res.json())
+.then(drink => renderDotdItem(drink))
+
+function renderDotdItem(drink) {
+    let drinkName = document.querySelector('.dotd_name');
+    let drinkImg = document.querySelector('.dotd_image');
+    let drinkLikes = document.querySelector('.dotd_rating');
+
+    drinkName.innerText = drink.name;
+    drinkImg.src = drink.img;
+    drinkLikes.innerText = parseInt(drink.likes);
+
+    upvote.addEventListener ('click', () => {
+        console.log('upvote clicked');
+        console.log(drinkLikes.innerText);
+        drinkLikes +=1;
+     })
+    
+     downvote.addEventListener ('click', () => {
+        console.log('downvote clicked');
+        console.log(drinkLikes.innerText);
+        drinkLikes -=1;
+     })
+};
+
  
-upvote.addEventListener ('click', () => {
-   console.log('upvote clicked');
-   parseInt.rating +=1;
-}) 
 downvote.addEventListener ('click', () => {
-    downvoteDOTD();
+    // downvoteDOTD();
 })
 // function downvoteDOTD(
 
 // function upvoteDOTD();
-
-console.log(upvote)

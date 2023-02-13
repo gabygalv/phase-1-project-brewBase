5eeaf5b6fe42d0952914bebf4d5a8bf334b14f93
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-13 13:36:01 -0700

    added data to db.json to test the up/down votes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const brewAPI = "https://api.openbrewerydb.org/breweries";
 const listContainer = document.querySelectorAll('.brew_list');
 const byId = (id) => document.getElementById(id);
+const upvote = document.querySelector('.dotd_like');
+const downvote = document.querySelector('.dotd_dislike');
+let rating = document.querySelector('.count');
 
 fetch(brewAPI)
  .then(res => res.json())
@@ -24,6 +27,27 @@ fetch(brewAPI)
 
 function breweryDetails(brewery) {
     //  console.log(brewery);
+    const breweryName = ''
+    const breweryType =''
+    const breweryAddress = ''
+    const breweryPhone = ''
+
 
 };
 
+/////////////////////////
+//DOTD stuff//
+/////////////////////////
+ 
+upvote.addEventListener ('click', () => {
+   console.log('upvote clicked');
+   parseInt.rating +=1;
+}) 
+downvote.addEventListener ('click', () => {
+    downvoteDOTD();
+})
+// function downvoteDOTD(
+
+// function upvoteDOTD();
+
+console.log(upvote)

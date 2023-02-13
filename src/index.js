c424e95a055fae8fcadb87c63e920859422ce6c1
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-13 11:31:37 -0700

    adding some comments and breweryDetails function

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 const brewAPI = "https://api.openbrewerydb.org/breweries";
-const brewList = document.querySelectorAll('#list_container');
+const listContainer = document.querySelectorAll('.brew_list');
 const byId = (id) => document.getElementById(id);
 
 fetch(brewAPI)
  .then(res => res.json())
  .then(breweries => oneBrewery(breweries))
 
+ //iterates through the breweries array
  function oneBrewery(breweries) {
     breweries.forEach(listBreweries);
  }
 
+ //creates an li for each brewery in the breweries array
+ //event listener starts the breweryDetails function
  function listBreweries(brewery) {
-    console.log(brewery);
-    const breweryName = document.createElement('h2')
-    breweryName.innerText = brewery.name
-    console.log(breweryName)
-    brewList.appendChild(breweryName);
+    // console.log(brewery);
+    const breweryName = document.createElement('li');
+    breweryName.innerText = brewery.name;
+    // console.log(breweryName);
+    breweryName.addEventListener('click', () => breweryDetails(brewery));
+    //listContainer.appendChild(breweryName);
 }
 
 function breweryDetails(brewery) {
-    // console.log(brewery);
+    //  console.log(brewery);
 
 };
 
